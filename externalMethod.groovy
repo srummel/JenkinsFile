f1ed8f200097e2793e24c5ccb4a67b80d6dff6f9
Shawn Rummel <srummel@servicemaster.com>
2017-01-18 11:24:30 -0600

    remove my var definition

--- externalMethod.groovy
+++ externalMethod.groovy
@@ -1,4 +1,3 @@
-def myVar = build.getEnvironment(listener).get('myVar')
 
 /// Methods in this file will end up as object methods on the object that load returns.
 def lookAtThis(String whoAreYou) {
