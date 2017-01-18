cfca4267cce6fdb410cb37693a897d4b300643c3
Shawn Rummel <srummel@servicemaster.com>
2017-01-18 11:06:12 -0600

    added println to groovy code

--- org/foo/Utilities.groovy
+++ org/foo/Utilities.groovy
@@ -2,6 +2,6 @@ package org.foo
 
 class Utilities {
   static def mvn(script, args) {
-    script.sh "${script.tool 'Maven'}/bin/mvn -s ${script.env.HOME}/jenkins.xml -o ${args}"
+   println("IN UTILITIES **1")
   }
 }
