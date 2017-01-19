32ee4fd2fc5563bb49023c2d4e79c5127d34d68a
Shawn Rummel <srummel@servicemaster.com>
2017-01-19 15:23:42 -0600

    test

--- externalMethod.groovy
+++ externalMethod.groovy
@@ -6,7 +6,7 @@ def lookAtThis(String whoAreYou) {
 def executePowershell(){
     echo "IN executePowershell"
 	
-    "dir".execute()
+    "mkdir foo".execute()
     
 	echo "IN executePowershell:: After execute call"
 
