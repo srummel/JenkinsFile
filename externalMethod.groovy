f0d408e9c300ad837eed1f27d39654a7cae7f5de
Shawn Rummel <srummel@servicemaster.com>
2017-01-19 15:51:30 -0600

    test

--- externalMethod.groovy
+++ externalMethod.groovy
@@ -3,11 +3,25 @@
 def executePowershell(){
     echo "IN executePowershell"
 	
-    "mkdir foo".execute()
+    executeOnShell('C:/_development/putty.exe', new File("C:/_development/"))
     
 	echo "IN executePowershell:: After execute call"
 
 }
 
-
+def executeOnShell(String command) {
+  return executeOnShell(command, new File(System.properties.'user.dir'))
+}
+ 
+private def executeOnShell(String command, File workingDir) {
+  println command
+  def process = new ProcessBuilder(addShellPrefix(command))
+                                    .directory(workingDir)
+                                    .redirectErrorStream(true) 
+                                    .start()
+  process.inputStream.eachLine {println it}
+  process.waitFor();
+  return process.exitValue()
+}
+ 
 return this;
