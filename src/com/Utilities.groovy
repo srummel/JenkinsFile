86e474cedf803a980fd1d40cf10e76aa0318004e
Shawn Rummel <srummel@servicemaster.com>
2017-01-19 10:14:27 -0600

    changed echo message

--- src/com/Utilities.groovy
+++ src/com/Utilities.groovy
@@ -1,8 +1,7 @@
 
 /// Methods in this file will end up as object methods on the object that load returns.
 static def hello(String uuid) {
-echo uuid
+    echo "Look at this, ${uuid}! You loaded this from another file!"
 }
-
 return this;
    
