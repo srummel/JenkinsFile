cf1f759421bd8e880cfe73fd95e002d767b2c6aa
Shawn Rummel <srummel@servicemaster.com>
2017-01-20 15:56:31 -0600

    sdf

--- externalMethod.groovy
+++ externalMethod.groovy
@@ -1,9 +1,11 @@
 // Methods in this file will end up as object methods on the object that load returns.
+import java.io.File 
+
 
 def updatePacLog(text){
     echo "** externalMethod.updatePacLog::Start **"
 
-	File f = new File('PAC.log')
+	File f = new File("PAC.log")
     f.append(text)
 	
     echo "** externalMethod.updatePacLog::Finish **"
