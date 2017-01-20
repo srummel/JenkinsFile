b8baccf8fc6a0bffd9f408679375500e917fd21a
Shawn Rummel <srummel@servicemaster.com>
2017-01-20 11:05:34 -0600

    TEst

--- externalMethod.groovy
+++ externalMethod.groovy
@@ -10,7 +10,7 @@ def executePowershell(){
 //  pb. redirectErrorStream(true);
 
 //  Process p = pb.start();
-  Process p = Runtime.getRuntime().exec("First.bat", null , new File("C:/tmp"));
+  Process p = Runtime.getRuntime().exec("mkdir c:/tmp/foo/1", null , new File("C:/tmp"));
   
 	echo "IN executePowershell:: After execute call"
 
