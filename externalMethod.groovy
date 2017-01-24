a5b6bbeb17546b56463f7b1650aad1064e3493ff
Shawn Rummel <srummel@servicemaster.com>
2017-01-24 13:31:13 -0600

    fixed file declaration.... again

--- externalMethod.groovy
+++ externalMethod.groovy
@@ -5,10 +5,9 @@ import java.io.File
 def updatePacLog(text){
 
     echo "** externalMethod.updatePacLog::Start **"
-Calendar cal=Calendar.getInstance();
 
-
-	File f.append(new File(getClass().protectionDomain.codeSource.location.path).absolutePath + 'PAC.log\r\n')
+	File f = new File("C:/tmp/PAC.log")
+	f.append(new File(getClass().protectionDomain.codeSource.location.path).absolutePath + '\r\n')
     f.append(text + '\r\n')
 	
     echo "** externalMethod.updatePacLog::Finish **"
@@ -17,9 +16,12 @@ def startStage(){
 
     echo "** externalMethod.startStage::Start **"
 
-	File f.append(new File(getClass().protectionDomain.codeSource.location.path).absolutePath + 'PAC.log\r\n')
+	File f = new File("C:/tmp/PAC.log")
+	f.append(new File(getClass().protectionDomain.codeSource.location.path).absolutePath + 'PAC.log\r\n')
 	
     echo "** externalMethod.startStage::Finish **"
 }
  
 return this;
+
+    echo "** externalMethod.updatePacLog::Finish **"
