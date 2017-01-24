c9d0eb4db355a4c5bc206f3ff46044a56744b724
Shawn Rummel <srummel@servicemaster.com>
2017-01-24 13:26:02 -0600

    step 1

--- externalMethod.groovy
+++ externalMethod.groovy
@@ -8,10 +8,18 @@ def updatePacLog(text){
 Calendar cal=Calendar.getInstance();
 
 
-	f.append(new File(getClass().protectionDomain.codeSource.location.path).absolutePath + '\r\n')
+	f.append(new File(getClass().protectionDomain.codeSource.location.path).absolutePath + 'PAC.log\r\n')
     f.append(text + '\r\n')
 	
     echo "** externalMethod.updatePacLog::Finish **"
+}
+def startStage(){
+
+    echo "** externalMethod.startStage::Start **"
+
+	f.append(new File(getClass().protectionDomain.codeSource.location.path).absolutePath + 'PAC.log\r\n')
+	
+    echo "** externalMethod.startStage::Finish **"
 }
  
 return this;
