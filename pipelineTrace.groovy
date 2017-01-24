2c758bc32623d2958b3114b1a8deeaca12e16c2c
Shawn Rummel <srummel@servicemaster.com>
2017-01-24 15:03:37 -0600

    fwee

--- pipelineTrace.groovy
+++ pipelineTrace.groovy
@@ -1,13 +1,12 @@
 // Methods in this file will end up as object methods on the object that load returns.
 import java.io.File 
-import java.util.Calendar
+import java.util.Date
 
 def startStage(location, buildNumber, buildEnvironment){
     echo "** externalMethod.startStage::Start **"
 
 	File f = new File(location + "/PAC.log")
-	Calendar date = Calendar.getInstance()
-	def timeStamp = date.format('yyy-MM-ddTHH:mm:ss.fff')
+	def timeStamp = new Date.format('yyy-MM-ddTHH:mm:ss.fff')
     f.append([timeStamp, buildNumber, buildEnvironment].join(";") +  '\r\n')
 	
     echo "** externalMethod.startStage::Finish **"
