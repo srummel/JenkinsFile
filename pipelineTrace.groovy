cb5b111d2dd56aac6d51cfae29749c9834956f7f
Shawn Rummel <srummel@servicemaster.com>
2017-01-25 15:26:50 -0600

    srgs

--- pipelineTrace.groovy
+++ pipelineTrace.groovy
@@ -24,10 +24,19 @@ def output(text){
     echo "** pipelineTrace.endStage::" + stage + "::Start **"
 
 	File f = new File(location + "/PAC.log")
-	def timeStamp = new Date().format('yyyy-MM-dd HH:mm:ss.SSS')
-    f.append([timeStamp, buildNumber, buildEnvironment].join("; ") + " ::Complete " + stage + '\r\n')
+    f.append(text + '\r\n')
 	
     echo "** pipelineTrace.endStage::" + stage + "::Finish **"
+}
+
+def logError(text){
+    echo "** pipelineTrace.logError::" + stage + "::Start **"
+
+	File f = new File(location + "/PAC.log")
+	def timeStamp = new Date().format('yyyy-MM-dd HH:mm:ss.SSS')
+    f.append([timeStamp, buildNumber, buildEnvironment].join("; ") + " ::End " + stage + '\r\n')
+	
+    echo "** pipelineTrace.logError::" + stage + "::Finish **"
 }
 
 return this;
