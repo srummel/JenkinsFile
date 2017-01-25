de1e70b0734a297a6b327027cf3c6f0755d454e4
Shawn Rummel <srummel@servicemaster.com>
2017-01-25 15:07:16 -0600

    sdvdv

--- pipelineTrace.groovy
+++ pipelineTrace.groovy
@@ -20,4 +20,14 @@ def endStage(location, buildNumber, buildEnvironment, stage){
     echo "** pipelineTrace.endStage::" + stage + "::Finish **"
 }
 
+def output(text){
+    echo "** pipelineTrace.endStage::" + stage + "::Start **"
+
+	File f = new File(location + "/PAC.log")
+	def timeStamp = new Date().format('yyyy-MM-dd HH:mm:ss.SSS')
+    f.append([timeStamp, buildNumber, buildEnvironment].join("; ") + " ::Complete " + stage + '\r\n')
+	
+    echo "** pipelineTrace.endStage::" + stage + "::Finish **"
+}
+
 return this;
