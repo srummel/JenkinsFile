5a8d26c957ecb808b16126991fb3584b8a6f9a09
Shawn Rummel <srummel@servicemaster.com>
2017-01-25 09:04:42 -0600

    sdved

--- pipelineTrace.groovy
+++ pipelineTrace.groovy
@@ -8,6 +8,16 @@ def startStage(location, buildNumber, buildEnvironment, stage){
 	
     echo "** externalMethod.startStage::" + stage + "::Finish **"
 }
-    return this;
 
-    echo "** externalMethod.updatePacLog::" + stage + "::Finish **"
+
+def endStage(location, buildNumber, buildEnvironment, stage){
+    echo "** externalMethod.endStage::Start **"
+
+	File f = new File(location + "/PAC.log")
+	def timeStamp = new Date().format('yyyy-MM-dd HH:mm:ss.SSS')
+    f.append([timeStamp, buildNumber, buildEnvironment].join("; ") + "Complete " + stage + '\r\n')
+	
+    echo "** externalMethod.endStage::" + stage + "::Finish **"
+}
+
+return this;
